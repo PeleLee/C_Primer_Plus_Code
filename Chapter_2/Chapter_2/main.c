baaad3a7eb4bfe171c4751535a3c3ac5e0948122
IlreldI <804024833@qq.com>
2019-04-13 23:11:25 +0800

    2.1

---
+++ Chapter_2/Chapter_2/main.c
@@ -0,0 +1,28 @@
+//
+//  main.c
+//  Chapter_2
+//
+//  Created by LiQunFei on 2019/4/13.
+//  Copyright © 2019 IlreldI. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    
+    // 2.1.1
+    /*
+     希望能运行。
+    int x = 100;
+    int y = 200;
+    /*其他内容已省略。*/
+    
+    // 2.1
+    /*int num;
+    num = 1;
+    printf("I'm a simple ");
+    printf("computer.\n");
+    printf("My favorite number is %d because it is first.\n",num);*/
+    
+    return 0;
+}
