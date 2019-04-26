210fc99112d600f3aaee43d8995cc9cc6b35d485
IlreldI <804024833@qq.com>
2019-04-26 23:20:49 +0800

    7.7-7.9

--- Chapter_7/Chapter_7/main.m
+++ Chapter_7/Chapter_7/main.m
@@ -9,6 +9,7 @@
 #import <Foundation/Foundation.h>
 #include <ctype.h>
 #include <stdbool.h>
+#include <stdio.h>
 
 #define SPACE ' '
 #define RATE1 0.13230
@@ -23,6 +24,10 @@
 #define BASE3 (BASE1+BASE2+(BREAK3*(BREAK3-BREAK2)))
 
 #define PERIOD '.'
+
+#define STOP '|'
+
+#define COVERAGE 350 //每罐油漆可刷的面积
 
 int main(int argc, const char * argv[]) {
     
@@ -40,9 +45,87 @@ int main(int argc, const char * argv[]) {
      7.2.5 多层嵌套的if语句
      
      7.3 逻辑运算符
+     7.3.1 备选拼写:iso646.h头文件
+     7.3.2 优先级
+     7.3.3 求值顺序
+     7.3.4 范围
+     
+     7.4 一个统计单词的程序
+     
+     7.5 条件运算符:?:
+     
+     7.6 循环辅助: continue和break
      */
     
     /* --Code-- */
+    
+    // 7.9 使用contiune跳过部分循环
+    /*sconst float MIN = 0.0f;
+    const float MAX = 100.0f;
+    float score;
+    float total = 0.0f;
+    int n = 0;
+    float min = MAX;
+    float max = MIN;
+    printf("Enter the first score (q to quit): ");
+    while (scanf("%f", &score) == 1) {
+        if (score < MIN || score > MAX) {
+            printf("%0.1f is an invalid value. Try again:", score);
+            continue;
+        }
+        printf("Accepting %0.1f:\n", score);
+        // 求最大最小值
+        min = (score < min) ? score : min;
+        max = (score > max) ? score : max;
+        total += score;
+        n++;
+        printf("Enter nex score (q to quit): ");
+    }
+    if (n > 0) {
+        printf("Average of %d scores is %0.1f.\n", n, total/n);
+        printf("Low = %0.1f, high = %0.1f\n", min, max);
+    }
+    else {
+        printf("No valid scores were entered.\n");
+    }*/
+    
+    // 7.8 使用条件运算符
+    /*int sq_feet;
+    int cans;
+    printf("Enter number of square feet to be painted:\n");
+    while (scanf("%d", &sq_feet) == 1) {
+        cans = sq_feet/COVERAGE;
+        cans += ((sq_feet % COVERAGE == 0) ? 0 : 1);
+        printf("You need %d %s of paint.\n", cans, cans == 1 ? "can" : "cans");
+        printf("Enter next value (q to quit):\n");
+    }*/
+    
+    // 7.7 -- 统计字符数、单词数、行数
+    /*char c;
+    char prev;
+    long n_chars = 0L;// 字符数
+    int n_lines = 0;// 行数
+    int n_words = 0;// 单词数
+    int p_lines = 0;// 不完整的行数
+    bool inword = false; // 如果c在单词中，inword等于true
+    printf("Enter text to be analyzed(|to terminate):\n");
+    prev = '\n';// 用于识别完整的行
+    while ((c = getchar()) != STOP) {
+        n_chars++;// 统计字符
+        if (c == '\n')
+            n_lines++; // 统计行
+        if (!isspace(c) && !inword) {
+            inword = true;// 开始一个新的单词
+            n_words++;// 统计单词
+        }
+        if (isspace(c) && inword)
+            inword = false;// 打到单词的末尾
+        prev = c;// 保存字符的值
+    }
+    if (prev != '\n')
+        p_lines = 1;
+    printf("characters = %ld, words = %d, lines = %d, ", n_chars, n_words, n_lines);
+    printf("partial lines = %d \n", p_lines);*/
     
     // 7.6 -- 使用逻辑与运算符
     /*char ch;
