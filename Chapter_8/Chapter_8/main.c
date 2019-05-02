35a5adf88289ef4e676aa5855c66ae5ba7fa6d03
IlreldI <804024833@qq.com>
2019-05-02 23:55:09 +0800

    8.5-8.7

--- Chapter_8/Chapter_8/main.c
+++ Chapter_8/Chapter_8/main.c
@@ -8,6 +8,15 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
+
+void display(char cr, int lines, int width);
+
+long get_long(void);
+
+bool bad_limits(long begin, long end, long low, long high);
+
+double sum_squares(long a, long b);
 
 int main(int argc, const char * argv[]) {
     /*--摘要--*/
@@ -28,11 +37,78 @@ int main(int argc, const char * argv[]) {
      8.5 创建更友好的用户界面
      8.5.1 使用缓冲输入
      8.5.2 混合数值和字符输入
+     
+     8.6 输入验证
+     8.6.1 分析程序
+     8.6.2 输入流和数字
+     
+     8.7 菜单浏览
+     8.7.1 任务
+     8.7.2 使执行更顺利
      */
     /*--Code--*/
     
-    // 8.5 -- 有较大I/O问题的程序
+    // 8.7
+    /*const long MIN = -10000000L;
+    const long MAX = +10000000L;
+    long start;
+    long stop;
+    double answer;
+    printf("This program computes the sum of the squares of "
+           "integers in a range.\nThe lower bound should not "
+           "be less than -10000000 and \nthe upper bound "
+           "should not be more than +10000000.\nEnter the "
+           "limits (enter 0 for both limits to quit):\n"
+           "lower limit: ");
+    start = get_long();
+    printf("upper limit: ");
+    stop = get_long();
+    while (start != 0 || stop != 0) {
+        if (bad_limits(start, stop, MIN, MAX)) {
+            printf("Please try again.\n");
+        }
+        else {
+            answer = sum_squares(start, stop);
+            printf("The sum of the squares of the integers ");
+            printf("from %ld to %ld is %g\n", start, stop, answer);
+        }
+        printf("Enter the limits (enter 0 for both "
+               "limits to quit):\n");
+        printf("lower limit: ");
+        start = get_long();
+        printf("upper limit: ");
+        stop = get_long();
+    }
+    printf("Done.\n");*/
     
+    // 8.6
+    /*int ch;
+    int rows, cols;
+    printf("Enter a character and two integers:\n");
+    while ((ch = getchar()) != '\n') {
+        if (scanf("%d %d", &rows, &cols) != 2) {
+            break;
+        }
+        display(ch, rows, cols);
+        while (getchar() != '\n') {
+            continue;
+        }
+        printf("Enter another character and two integers;\n");
+        printf("Enter a newline to quit.\n");
+    }
+    printf("Bye.\n");*/
+    
+    // 8.5 -- 有较大I/O问题的程序
+    /*int ch;
+    int rows, cols;
+    printf("Enter a character and two integers:\n");
+    while ((ch = getchar()) != '\n') {
+        scanf("%d %d", &rows, &cols);
+        display(ch, rows, cols);
+        printf("Enter another character and two integers;\n");
+        printf("Enter a newline to quit.\n");
+    }
+    printf("Bye.\n");*/
     
     // 8.4.2
     /*int guess = 1;
@@ -108,4 +184,53 @@ int main(int argc, const char * argv[]) {
     }*/
     
     return 0;
+}
+
+bool bad_limits(long begin, long end, long low, long high) {
+    bool not_good = false;
+    if (begin > end) {
+        printf("%ld isn't smaller than %ld.\n", begin, end);
+        not_good = true;
+    }
+    if (begin < low || end < low) {
+        printf("Values must be %ld or greater.\n", low);
+        not_good = true;
+    }
+    if (begin > high || end > high) {
+        printf("Values must be %ld or less.\n", high);
+        not_good = true;
+    }
+    return not_good;
+}
+
+double sum_squares(long a, long b) {
+    double total = 0;
+    long i;
+    for (i = a; i <= b; i++) {
+        total += (double)i*(double)i;
+    }
+    return total;
+}
+
+long get_long(void) {
+    long input;
+    char ch;
+    while (scanf("%ld", &input) != 1) {
+        while ((ch = getchar()) != '\n') {
+            putchar(ch);
+        }
+        printf(" is not an integer.\nPlease enter an ");
+        printf("integer value, such as 25, -178, or 3:");
+    }
+    return input;
+}
+
+void display(char cr, int lines, int width) {
+    int row, col;
+    for (row = 1; row <= lines; row++) {
+        for (col = 1; col <= width; col++) {
+            putchar(cr);
+        }
+        putchar('\n');
+    }
 }
